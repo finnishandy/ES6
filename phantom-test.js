17840b896fecac980cc3532b8652b05dd54ded2a
Sakari.Ruoho <finnishandy@gmail.com>
2015-12-15 16:18:02 +0000

    phantom

---
+++ phantom-test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Sakari.Ruoho on 15/12/2015.
+ */
+var phantom = require('phantom');
+
+phantom.create(function (ph) {
+    ph.createPage(function (page) {
+        page.open("http://www.google.com", function (status) {
+            console.log("opened google? ", status);
+            page.evaluate(function () { return document.title; }, function (result) {
+                console.log('Page title is ' + result);
+                ph.exit();
+            });
+        });
+    });
+});
